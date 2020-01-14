1eb9a5db7439f6619ddb97396beaec16754920ce
Adam Leskis <leskis@gmail.com>
2020-01-14 08:13:24 +0000

    with random ip and result message

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Layout from './Layout';
 import '../styles/play.scss';
 import { useHistory } from 'react-router-dom';
+import { isPrivate, randomIp } from '../utils/generateIP';
 // import { useSwipeable } from 'react-swipeable';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight, faHome, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
 const Play = () => {
   // const handlers = useSwipeable();
+  const [ip, setIp] = useState(randomIp())
+  const [correctOrNot, setCorrectOrNot] = useState(null)
 
   const history = useHistory();
 
-  const handleClick = () => history.push("/");
+  const handleHomeClick = () => history.push("/");
+
+  const handleLeftArrowClick = () => {
+    const correct = isPrivate(ip);
+    setCorrectOrNot(correct);
+    setIp(randomIp());
+  }
+
+  const handleRightArrowClick = () => {
+    const correct = !isPrivate(ip); 
+    setCorrectOrNot(correct);
+    setIp(randomIp()); 
+  }
 
   return (
     <Layout>
       <div className="top-icons">
         <div className="home-icon">
-          <span onClick={handleClick}>
+          <span onClick={handleHomeClick}>
             <FontAwesomeIcon icon={faHome} />
           </span>
         </div>
@@ -27,13 +42,16 @@ const Play = () => {
         </div>
       </div>     
       <div className="main-game">
-        <div className="ip-address">92.33.1.14</div> 
+        <div className="ip-address">{ip}</div> 
+      </div>
+      <div className="result">
+        {correctOrNot ? "you got it!" : "wrong answer"}
       </div>
       <div className="swipe-arrows">
-        <div className="left-arrow">
+        <div className="left-arrow" onClick={handleLeftArrowClick}>
           <FontAwesomeIcon icon={faArrowLeft} />
         </div>
-        <div className="right-arrow">
+        <div className="right-arrow" onClick={handleRightArrowClick}>
           <FontAwesomeIcon icon={faArrowRight} />
         </div>
       </div>
