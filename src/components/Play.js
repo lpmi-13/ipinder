a2dcf770b7e2836aefed2ed38555109449b0c72c
Adam Leskis <leskis@gmail.com>
2020-01-15 22:23:52 +0000

    comment out iptracker for now

--- src/components/Play.js
+++ src/components/Play.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import Layout from './Layout';
 import '../styles/play.scss';
 import { useHistory } from 'react-router-dom';
-import { isPrivate, randomIp } from '../utils/generateIP';
+import { isPrivate, publicIp, privateIp, randomIp } from '../utils/generateIP';
 // import { useSwipeable } from 'react-swipeable';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight, faHome, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
@@ -12,21 +12,47 @@ const Play = () => {
   // const handlers = useSwipeable();
   const [ip, setIp] = useState(randomIp())
   const [correctOrNot, setCorrectOrNot] = useState(null)
+  const [ipTracker, setIpTracker] = useState([]);
 
   const history = useHistory();
 
+  const generateIp = () => {
+    // if we have at least one private IP address in the array of IP addresses
+    if(ipTracker.includes("true")) {
+      return publicIp();
+    } else {
+      return privateIp();
+    }
+  }
+
   const handleHomeClick = () => history.push("/");
 
+  // left clicks are for private IPs
   const handleLeftArrowClick = () => {
     const correct = isPrivate(ip);
     setCorrectOrNot(correct);
-    setIp(randomIp());
+    // we want to show at least one private IP every 5 times
+    if (ipTracker.length > 3) {
+      const reducedIp = ipTracker.slice(1, 4);
+      setIpTracker([...reducedIp, isPrivate(ip).toString()]);
+    } else {
+      setIpTracker([...ip, isPrivate(ip).toString()]);
+    }
+    setIp(generateIp());
   }
 
+  // right clicks are for public IPs
   const handleRightArrowClick = () => {
     const correct = !isPrivate(ip); 
     setCorrectOrNot(correct);
-    setIp(randomIp()); 
+    // we want to show at least one private IP every 5 times
+    if (ipTracker.length > 3) {
+      const reducedIp = ipTracker.slice(1, 4);
+      setIpTracker([...reducedIp, isPrivate(ip).toString()]);
+    } else {
+      setIpTracker([...ip, isPrivate(ip).toString()]);
+    }
+    setIp(generateIp()); 
   }
 
   return (
@@ -46,6 +72,9 @@ const Play = () => {
       </div>
       <div className="result">
         {correctOrNot ? "you got it!" : "wrong answer"}
+      </div>
+      <div>
+        {/* {ipTracker} */}
       </div>
       <div className="swipe-arrows">
         <div className="left-arrow" onClick={handleLeftArrowClick}>
--- src/utils/generateIP.js
+++ src/utils/generateIP.js
@@ -1,6 +1,6 @@
 const randomByte = () => Math.round(Math.random()*256);
 
-export const isPrivate = (ip) => {
+ export const isPrivate = (ip) => {
   return /^10\.|^192\.168\.|^172\.16\.|^172\.17\.|^172\.18\.|^172\.19\.|^172\.20\.|^172\.21\.|^172\.22\.|^172\.23\.|^172\.24\.|^172\.25\.|^172\.26\.|^172\.27\.|^172\.28\.|^172\.29\.|^172\.30\.|^172\.31\./.test(ip);
 }
   
@@ -11,4 +11,23 @@ export const randomIp = () => {
            randomByte();
 
     return ip;
+}
+
+export const publicIp = () => {
+  const ip = randomIp();
+  if (!isPrivate(ip)) {
+    return ip;
+  } else {
+    return publicIp();
+  }
+
+}
+
+export const privateIp = () => {
+  const ip = randomIp();
+  if (isPrivate(ip)) {
+    return ip;
+  } else {
+    return privateIp();
+  } 
 }
